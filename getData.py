8d954c3ff0012d71f69acb9378dd04aed5516c2a
Danillo <danillodesouza03@gmail.com>
2023-09-04 20:48:16 -0300

    Update getData.py
    Adicionando funções para tornar o código mais legível

--- getData.py
+++ getData.py
@@ -4,58 +4,27 @@ import time
 from bs4 import BeautifulSoup
 import pandas as pd
 
-driver = webdriver.Chrome()
+# Função para realizar a rolagem até o final da página
+def scroll_ate_final(driver):
+    atual_altura = driver.execute_script("return document.body.scrollHeight")
 
-# URL da página + termo de pesquisa
-value_term = "Dados"
-url = f"https://portal.gupy.io/job-search/term={value_term}"
+    while True:
+        # Rola até o final da página atual
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        
+        time.sleep(1)
+        
+        # Pega a altura depois da página ser rolada
+        nova_altura = driver.execute_script("return document.body.scrollHeight")
+        
+        # Verifica se chegou ao final 
+        if nova_altura == atual_altura:
+            break
+        
+        atual_altura = nova_altura
 
-# Abre a pag da web com o url definido na variavel
-driver.get(url)
-
-# Pega a altura da página antes de começar a rolagem
-atual_altura = driver.execute_script("return document.body.scrollHeight")
-
-while True:
-    # Rola até o final da página atual
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    
-    time.sleep(1)
-    
-    # Pega a altura depois da página ser rolada
-    nova_altura = driver.execute_script("return document.body.scrollHeight")
-    
-    # Vê se chegou ao final 
-    if nova_altura == atual_altura:
-        break
-
-    atual_altura = nova_altura
-
-time.sleep(2)
-
-# Armazena a página que foi rolada até o final, e traz para ser tratado pelo bs4
-page_source = driver.page_source
-site = BeautifulSoup(page_source, 'html.parser')
-
-# Criando um dicionário para armazenar os dados
-dict_vagas = {
-    'empresa':[],
-    'cargo':[],
-    'localizacao':[],
-    'modalidade':[],
-    'contrato':[],
-    'pcd':[],
-    'data':[]
-}
-
-# Pegando a lista onde tem as vagas
-cards_vagas = site.find('ul', class_="zVzmE")
-
-# Pegando todas vagas da lista de vagas
-card_vagas = cards_vagas.find_all('li')
-
-# Percorrendo vaga a vaga
-for vaga in card_vagas:
+# Função para extrair os dados de cada vaga
+def extrair_dados_vagas(vaga):
     # extrai os dados contido em cada vaga 
     empresa = vaga.find("p", class_="cQyvth").text
     cargo = vaga.find("h2", class_="XNNQK").text
@@ -63,30 +32,78 @@ for vaga in card_vagas:
     localizacao = dados_spans[0].text
     modalidade = dados_spans[1].text
     contrato = dados_spans[2].text
-    
+
     # Verifica se tem 4 itens de dados, se tiver adiciona o conteúdo
     if len(dados_spans) >= 4:
         pcd = dados_spans[3].text
     else:
-        pcd = "Não informado"    
-    
+        pcd = "Não informado"  
+
     # Adiciona somente os 10 ultimos caracteres que por padrão é a data xx/xx/xxxx
     data = vaga.find("p", class_="inqtnx").text[-10:]
 
-    # Coloca os dados de cada vaga no dict
-    dict_vagas["empresa"].append(empresa)
-    dict_vagas["cargo"].append(cargo)
-    dict_vagas["localizacao"].append(localizacao)
-    dict_vagas["modalidade"].append(modalidade)
-    dict_vagas["contrato"].append(contrato)
-    dict_vagas["pcd"].append(pcd)
-    dict_vagas["data"].append(data)
+    return {
+        'empresa': empresa,
+        'cargo': cargo,
+        'localizacao': localizacao,
+        'modalidade': modalidade,
+        'contrato': contrato,
+        'pcd': pcd,
+        'data': data
+    }
 
-# Transforma o dict preenchido em um df
-df = pd.DataFrame(dict_vagas)
+# Função principal para buscar e salvar os dados
+def main():
+    driver = webdriver.Chrome()
+    
+    # URL da página + termo de pesquisa
+    value_term = "Dados"
+    url = f"https://portal.gupy.io/job-search/term={value_term}"
+    
+    # Abre a pag da web com o url definido na variavel
+    driver.get(url)
+    
+    # Chama a função para rolar a página até o final
+    scroll_ate_final(driver)
+    
+    time.sleep(2)
+    
+    # Armazena a página que foi rolada até o final, e traz para ser tratado pelo bs4
+    page_source = driver.page_source
+    site = BeautifulSoup(page_source, 'html.parser')
+    
+    # Criando um dicionário para armazenar os dados
+    dict_vagas = {
+        'empresa':[],
+        'cargo':[],
+        'localizacao':[],
+        'modalidade':[],
+        'contrato':[],
+        'pcd':[],
+        'data':[]
+    }
 
-# Salva esse df em csv, usando o UTF-8 como linguagem, separando por ";" e sem index
-df.to_csv("base_dados_vagas.csv", encoding='utf-8', sep=';', index=False)
+    # Pegando a lista onde tem as vagas
+    cards_vagas = site.find('ul', class_="zVzmE")
 
-# Fecha o navegador
-driver.quit()
+    # Pegando todas vagas da lista de vagas
+    card_vagas = cards_vagas.find_all('li')
+
+    # Percorrendo vaga a vaga
+    for vaga in card_vagas:
+        vaga_data = extrair_dados_vagas(vaga)
+        # Coloca os dados de cada vaga no dict
+        for key, value in vaga_data.items():
+            dict_vagas[key].append(value)
+
+    # Transforma o dict preenchido em um df
+    df = pd.DataFrame(dict_vagas)
+    
+    # Salva esse df em csv, usando o UTF-8 como linguagem, separando por ";" e sem index
+    df.to_csv("base_dados_vagas.csv", encoding='utf-8', sep=';', index=False)
+    
+    # Fecha o navegador
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
